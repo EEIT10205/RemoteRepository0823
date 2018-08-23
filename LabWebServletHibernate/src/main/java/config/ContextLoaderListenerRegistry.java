86dfe191acec4071a88f6e88c60e349c9ae46c17
吳浩澐 <howard@eeit102>
2018-08-23 12:40:08 +0800

    test 123

--- LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
+++ LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
@@ -11,7 +11,7 @@ public class ContextLoaderListenerRegistry extends AbstractContextLoaderInitiali
 				new AnnotationConfigWebApplicationContext();
 		context.register(SpringJavaConfiguration.class);
 		return context;
-		
+		//123
 		//test
 		
 		//test2
