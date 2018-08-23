6bd8b6d8be3d225a870bbf7599d0a2f9ffc85561
User <User@M580V-NB27>
2018-08-23 12:34:12 +0800

    在ContextLoaderListenerRegistry 加入//test

--- LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
+++ LabWebServletHibernate/src/main/java/config/ContextLoaderListenerRegistry.java
@@ -11,5 +11,7 @@ public class ContextLoaderListenerRegistry extends AbstractContextLoaderInitiali
 				new AnnotationConfigWebApplicationContext();
 		context.register(SpringJavaConfiguration.class);
 		return context;
+		
+		//test
 	}
 }
